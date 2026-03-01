6b887a53306646524b8d794bbe97bfb3b8e87af5
Sushant Bhagat <97627983+sushant-bhagat@users.noreply.github.com>
2026-03-01 23:33:28 +0530

    Update HelloWorldServlet.java

--- src/main/java/com/geekcap/vmturbo/HelloWorldServlet.java
+++ src/main/java/com/geekcap/vmturbo/HelloWorldServlet.java
@@ -1,11 +1,14 @@
 package com.geekcap.vmturbo;
 
+import java.io.IOException;            // <--- Add this
+import java.io.PrintWriter;            // <--- Usually needed for Servlets
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class HelloWorldServlet extends HttpServlet 
+public class HelloWorldServlet extends HttpServle
 {
 @Override
   public void service( HttpServletRequest req, HttpServletResponse res ) throws IOException {
